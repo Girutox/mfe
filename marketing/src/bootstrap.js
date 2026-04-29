fb11d2715058aa898d8ed8a8ad79fb4a7a90e3a5
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2026-04-29 15:39:25 -0500

    Add scoped css logic, Add scoped router logic

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname: pathName } = history.location;
+
+      if (pathName !== nextPathName) {
+        history.push(nextPathName);
+      }
+    }
+  };
 }
 
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
   if (el) {
-    mount(el);
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
